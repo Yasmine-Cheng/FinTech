e9f2c53cf63c2275259fd786f0700693a83a7f32
Yasmine-Cheng <69775438+Yasmine-Cheng@users.noreply.github.com>
2021-04-07 16:07:48 +0800

    Delete HW3/example_code directory

--- HW3/example_code/js/js2.js
+++
@@ -1,8 +0,0 @@
-onscroll = function() {
-		    var scrollTop = document.documentElement.scrollTop;
-		    if (scrollTop > 250) {
-		        $(".header").hide();
-		     } else {
-		        $(".header").show();
-		    }
-		}
